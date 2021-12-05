529efa89cfc8e4fac4a20e1aeac5f1db49fd51ad
dylan fox <fox.dylan92@gmail.com>
2021-12-05 16:23:28 -0600

    testing

--- api/server.js
+++ api/server.js
@@ -102,8 +102,8 @@ app.get('/version',function(req, res) {
 
 
 const httpsServer = https.createServer({
-  key: fs.readFileSync('/etc/letsencrypt/live/dylanandemily2022.com-0001/privkey.pem'),
-  cert: fs.readFileSync('/etc/letsencrypt/live/dylanandemily2022.com-0001/fullchain.pem'),
+  key: fs.readFileSync('/etc/letsencrypt/archive/dylanandemily2022.com-0001/privkey.pem'),
+  cert: fs.readFileSync('/etc/letsencrypt/archive/dylanandemily2022.com-0001/fullchain.pem'),
   dhparam: fs.readFileSync("/var/www/host/dh-strong.pem")
 }, app);
 
